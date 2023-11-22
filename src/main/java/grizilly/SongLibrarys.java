7792b7cba2e3194bc48dfb2d26b5159378ba0f8c
AXBY12 <gagekace@gmail.com>
2023-11-21 20:23:55 -0500

    Let me know
    Just used extends keyword

--- src/main/java/grizilly/SongLibrarys.java
+++ src/main/java/grizilly/SongLibrarys.java
@@ -47,7 +47,7 @@ class MusicLibrary {
     }
 }
 
-public class MusicApp {
+public class SongLibrarys extends Playlist {
     public static void main(String[] args) {
         MusicLibrary musicLibrary = new MusicLibrary();
         List<Song> allSongsPlaylist = musicLibrary.createAllSongsPlaylist();
