f4e943d60ca8f5d8ccd520441e7b4e22fd211136
BryceLucas <147726637+BryceLucas@users.noreply.github.com>
2023-11-25 09:50:10 -0500

    MusicLibraryManager.java
    added code for metadata of songs into the library manager

--- src/main/java/grizilly/MusicLibraryManager.java
+++ src/main/java/grizilly/MusicLibraryManager.java
@@ -5,15 +5,59 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import org.jaudiotagger.audio.AudioFile;
+import org.jaudiotagger.audio.AudioFileIO;
+import org.jaudiotagger.tag.Tag;
+import org.jaudiotagger.tag.FieldKey;
 
 public class MusicLibraryManager {
 
+    private MusicLibrary musicLibrary;
+
+    public MusicLibraryManager() {
+        this.musicLibrary = new MusicLibrary();
+    }
+
     public static void main(String[] args) {
         String musicFolderPath = "Path_to_music_folder";
-        String libraryFolderPath = "Path_to_library_folder";
+        MusicLibraryManager manager = new MusicLibraryManager();
+        manager.updateLibraryWithMetadata(musicFolderPath);
+    }
 
-        File musicFolder = new File(musicFolderPath);
-        File libraryFolder = new File(libraryFolderPath);
+    public void updateLibraryWithMetadata(String directoryPath) {
+        File musicFolder = new File(directoryPath);
+        File[] musicFiles = musicFolder.listFiles((dir, name) -> name.toLowerCase().matches(".*\\.(mp3|wav|flac)"));
+
+        if (musicFiles != null) {
+            for (File musicFile : musicFiles) {
+                try {
+                    Song song = readMetadata(musicFile);
+                    musicLibrary.addSong(song);
+                    System.out.println("Added to library: " + song);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private Song readMetadata(File musicFile) throws Exception {
+        AudioFile audioFile = AudioFileIO.read(musicFile);
+        Tag tag = audioFile.getTag();
+        String title = tag.getFirst(FieldKey.TITLE);
+        String artist = tag.getFirst(FieldKey.ARTIST);
+        String album = tag.getFirst(FieldKey.ALBUM);
+        String year = tag.getFirst(FieldKey.YEAR);
+        String genre = tag.getFirst(FieldKey.GENRE);
+        int lengthInSeconds = audioFile.getAudioHeader().getTrackLength();
+
+        return new Song(musicFile.getAbsolutePath(), title, artist, album, year, genre, lengthInSeconds);
+    }
+
+    // This method can be used if you want to copy music files to a library directory
+    private void copyMusicFiles(String sourceDir, String targetDir) throws IOException {
+        File musicFolder = new File(sourceDir);
+        File libraryFolder = new File(targetDir);
 
         if (!musicFolder.exists() || !libraryFolder.exists()) {
             System.out.println("One or both folders do not exist. Please check your paths.");
@@ -24,14 +68,10 @@ public class MusicLibraryManager {
         if (musicFiles != null) {
             for (File musicFile : musicFiles) {
                 if (musicFile.isFile() && isMusicFile(musicFile)) {
-                    try {
-                        Path sourcePath = musicFile.toPath();
-                        Path targetPath = new File(libraryFolder, musicFile.getName()).toPath();
-                        Files.copy(sourcePath, targetPath, StandardCopyOption.REPLACE_EXISTING);
-                        System.out.println("Copied: " + musicFile.getName());
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                    Path sourcePath = musicFile.toPath();
+                    Path targetPath = new File(libraryFolder, musicFile.getName()).toPath();
+                    Files.copy(sourcePath, targetPath, StandardCopyOption.REPLACE_EXISTING);
+                    System.out.println("Copied: " + musicFile.getName());
                 }
             }
         }
