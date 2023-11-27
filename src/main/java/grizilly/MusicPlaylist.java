0a4c5c0fa3cbb04f8d376f1c76ce1061376c9a56
kyle-gardner <kyletgardner18@gmail.com>
2023-11-27 00:03:23 -0500

    create musicPlaylist metadata

--- src/main/java/grizilly/MusicPlaylist.java
+++ src/main/java/grizilly/MusicPlaylist.java
@@ -1,39 +1,40 @@
 package grizilly;
 
-import java.util.ArrayList;
-import java.util.List;
 
-class MusicLibrary {
-    private List<Song> songs;
+// class MusicLibrary {
+//     private List<Song> songs;
 
-    public MusicLibrary() {
-        this.songs = new ArrayList<>();
-        LoadSongs();
-    }
-    public void addSong(Song song) {
-        songs.add(song);
-    }
-    private void LoadSongs() {
-        //songs.add(new Song("path", "title", "artist", 60));
-        // Add more songs as needed (dont know if this will automate)
-    }
+//     public MusicLibrary() {
+//         this.songs = new ArrayList<>();
+//         LoadSongs();
+//     }
+//     public void addSong(Song song) {
+//         songs.add(song);
+//     }
+//     private void LoadSongs() {
+//         //songs.add(new Song("path", "title", "artist", 60));
+//         // Add more songs as needed (dont know if this will automate)
+//     }
 
-    public List<Song> getAllSongs() {
-        return songs;
-    }
+//     public List<Song> getAllSongs() {
+//         return songs;
+//     }
 
-    public List<Song> createAllSongsPlaylist() {
-        return new ArrayList<>(songs);
-    }
-}
+//     public List<Song> createAllSongsPlaylist() {
+//         return new ArrayList<>(songs);
+//     }
+// }
 
 public class MusicPlaylist extends Playlist {
-    public static void main(String[] args) {
-        MusicLibrary musicLibrary = new MusicLibrary();
-        List<Song> allSongsPlaylist = musicLibrary.createAllSongsPlaylist();
-        System.out.println("All Songs Playlist:");
-        for (Song song : allSongsPlaylist) {
-            System.out.println(song);
-        }
-    }
+	public MusicPlaylist() {
+
+	}
+    // public static void main(String[] args) {
+    //     MusicLibrary musicLibrary = new MusicLibrary();
+    //     List<Song> allSongsPlaylist = musicLibrary.createAllSongsPlaylist();
+    //     System.out.println("All Songs Playlist:");
+    //     for (Song song : allSongsPlaylist) {
+    //         System.out.println(song);
+    //     }
+    // }
 }
