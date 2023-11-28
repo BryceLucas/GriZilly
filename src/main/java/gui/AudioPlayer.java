4d84474882221103a8370860c56b6843a484848a
Fireco82 <147770655+Fireco82@users.noreply.github.com>
2023-11-27 21:13:35 -0500

    Update AudioPlayer.java
    stop,Volume,unmute,and mute added

--- src/main/java/gui/AudioPlayer.java
+++ src/main/java/gui/AudioPlayer.java
@@ -5,26 +5,53 @@ import java.net.URI;
 
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaPlayer.Status;
+import javafx.scene.media.MediaView;
+import javafx.scene.media.AudioEqualizer;
 
 public class AudioPlayer {
-	String path;
-	MediaPlayer player;
+    String path;
+    MediaPlayer player;
 
-	public AudioPlayer(String path) {
-		this.path = path;
-		// ugly. could fix this in the musicPlaylistBuilder.java file
-		// has to be done because in a valid URI, the first : character needs to go after the format.
-		File f = new File(path);
-		URI u = f.toURI();
-		path = u.toString();
-		
-		Media media = new Media(path);
-		player = new MediaPlayer(media);
-	}
-	public void playAudio() {
-		player.play();
-	}
-	public void pauseAudio() {
-		player.pause();
-	}
+    public AudioPlayer(String path) {
+        this.path = path;
+        File f = new File(path);
+        URI u = f.toURI();
+        this.path = u.toString();
+
+        Media media = new Media(this.path);
+        player = new MediaPlayer(media);
+    }
+
+    public void playAudio() {
+        if (player.getStatus() == Status.PAUSED) {
+            player.play();
+        } else {
+            player.play();
+        }
+    }
+
+    public void pauseAudio() {
+        player.pause();
+    }
+
+    public void stopAudio() {
+        player.stop();
+    }
+
+    public void setVolume(double volume) {
+        player.setVolume(volume);
+    }
+
+    public double getVolume() {
+        return player.getVolume();
+    }
+
+    public void mute() {
+        player.setMute(true);
+    }
+
+    public void unmute() {
+        player.setMute(false);
+    }
 }
